0cca278f3397a8515e8713a1275e567e21c4e253
thientv7 <thientv7@viettel.com.vn>
2020-09-16 15:55:43 +0700

    Add code main activity

--- app/src/main/java/com/example/mvvmapplication/ui/main/adapter/MainAdapter.kt
+++ app/src/main/java/com/example/mvvmapplication/ui/main/adapter/MainAdapter.kt
@@ -1,24 +1,44 @@
 package com.example.mvvmapplication.ui.main.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.mvvmapplication.R
 import com.example.mvvmapplication.data.model.User
+import kotlinx.android.synthetic.main.item_layout.view.*
 
 class MainAdapter(private val users: ArrayList<User>): RecyclerView.Adapter<MainAdapter.DataViewHolder>() {
     class DataViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(user: User) {
+            itemView.apply {
+                textViewUserName.text = user.name
+                textViewUserEmail.text = user.email
 
+                Glide.with(imageViewAvatar.context)
+                    .load(user.avatar)
+                    .into(imageViewAvatar)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder {
-        TODO("Not yet implemented")
+        return DataViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false))
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return users.size
     }
 
     override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(users[position])
+    }
+
+    fun addUsers(users: List<User>){
+        this.users.apply {
+            clear()
+            addAll(users)
+        }
     }
 }
--- app/src/main/java/com/example/mvvmapplication/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvmapplication/ui/main/view/MainActivity.kt
@@ -2,16 +2,81 @@ package com.example.mvvmapplication.ui.main.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mvvmapplication.R
+import com.example.mvvmapplication.data.api.ApiHelper
+import com.example.mvvmapplication.data.api.RetrofitBuilder
+import com.example.mvvmapplication.data.model.User
+import com.example.mvvmapplication.ui.base.ViewModelFactory
+import com.example.mvvmapplication.ui.main.adapter.MainAdapter
 import com.example.mvvmapplication.ui.main.viewmodel.MainViewModel
+import com.example.mvvmapplication.utils.Status
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: MainAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setupViewModel(){
+        viewModel = ViewModelProviders.of(
+            this,
+            ViewModelFactory(ApiHelper(RetrofitBuilder.apiService))
+        ).get(MainViewModel::class.java)
+    }
+
+    private fun setupUi(){
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MainAdapter(arrayListOf())
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(
+                recyclerView.context,
+                (recyclerView.layoutManager as LinearLayoutManager).orientation
+            )
+        )
+        recyclerView.adapter = adapter
+    }
+
+    private fun setupObservers(){
+        viewModel.getUsers().observe(this, Observer {
+            it?.let {resource ->
+                when (resource.status){
+                    Status.SUCCESS -> {
+                        recyclerView.visibility = View.VISIBLE
+                        progressBar.visibility = View.GONE
+                        resource.data?.let { users -> retrieveList(users) }
+                    }
+                    Status.ERROR -> {
+                        recyclerView.visibility = View.VISIBLE
+                        progressBar.visibility = View.GONE
+                        Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                    }
+                    Status.LOADING -> {
+                        recyclerView.visibility = View.VISIBLE
+                        progressBar.visibility = View.GONE
+                        Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+
+            }
+        })
+    }
+
+    private fun retrieveList(users: List<User>){
+        adapter.apply {
+            addUsers(users)
+            notifyDataSetChanged()
+        }
     }
 
 
